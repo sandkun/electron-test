1174131ba7842b4412aa9b01357b2ac8da6ed1aa
조유민 <youminjo6559@gmail.com>
2024-08-21 16:15:37 +0900

    feat: 다국어처리 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useTranslation } from 'react-i18next';
+import i18n from './lang/locale';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [t] = useTranslation();
+
+	const change = () => {
+		i18n.changeLanguage(i18n.language === 'ko' ? 'en' : 'ko');
+		console.log('??');
+	};
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p onClick={change}>{t('helloWorld')}</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
